13a02bf1cffd694bd5e84a8b6c4a0cdf084d9f6e
4679 <btchina@live.com>
2016-01-20 10:17:47 +0800

    updated weather.sh

--- weather.sh
+++ weather.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 key="这里填写你的key"
 
-weatherinfo=$(curl -s "http://www.weather.com.cn/adat/cityinfo/101250105.html" | jq .weatherinfo)
-city=$(echo $weatherinfo | jq .city)
-weather=$(echo $weatherinfo | jq .weather)
-temp1=$(echo $weatherinfo | jq .temp1)
-temp2=$(echo $weatherinfo | jq .temp2)
+weatherinfo=$(curl -s "http://api.map.baidu.com/telematics/v3/weather?location=%E6%9C%9B%E5%9F%8E&output=json&ak=6dd11fd43e63dde2abc6bc9564aa37b3")
+weather_today=$(echo $weatherinfo | jq .results | jq '.[].weather_data' | jq '.[0]')
+city=$(echo $weatherinfo | jq .results | jq '.[].currentCity') && city=$(echo ${city#\"}) && city=$(echo ${city%\"})
 
-city=$(echo ${city#\"}) && city=$(echo ${city%\"})
-weather=$(echo ${weather#\"}) && weather=$(echo ${weather%\"})
-temp1=$(echo ${temp1#\"}) && temp1=$(echo ${temp1%\"})
-temp2=$(echo ${temp2#\"}) && temp2=$(echo ${temp2%\"})
+day_pic=$(echo $weather_today | jq .dayPictureUrl) && day_pic=$(echo ${day_pic#\"}) && day_pic=$(echo ${day_pic%\"})
+night_pic=$(echo $weather_today | jq .nightPictureUrl) && night_pic=$(echo ${night_pic#\"}) && night_pic=$(echo ${night_pic%\"})
+weather=$(echo $weather_today | jq .weather) && weather=$(echo ${weather#\"}) && weather=$(echo ${weather%\"})
+wind=$(echo $weather_today | jq .wind) && wind=$(echo ${wind#\"}) && wind=$(echo ${wind%\"})
+temp=$(echo $weather_today | jq .temperature) && temp=$(echo ${temp#\"}) && temp=$(echo ${temp%\"})
 
 title="Server醬"
-content="变态 今天$city的天气是:$weather 最高气温$temp1 最低气温$temp2 
+content="变态 今天$city的天气是:$weather 气温$temp $wind
+
+
+![day]($day_pic)
+![night]($night_pic)
 
 
 才 才不是特意提醒你的呢 哼~
